fbad93d752c4878a9364c0946b25cecb4c3ba6b3
Khushboo-19kz <zopek@coventry.ac.uk>
2026-03-21 19:32:14 +0000

    Loaded Datasets

--- code/Question_A.py
+++ code/Question_A.py
@@ -1,1 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read/Load datasets
+
+Trips_by_Distance = pd.read_csv("Trips_by_Distance.csv")
+Trips_Full Data = pd.read_csv("Trips_Full Data.csv")
